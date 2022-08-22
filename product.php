993887ffc4a824c88942bc647727f7918ceb54dc
Mamat1411 <muhammadmujahid14@gmail.com>
2022-08-22 16:30:12 +0700

    Simple Method

--- product.php
+++ product.php
@@ -1,13 +1,18 @@
 <?php
 
 class Product{
-    public $title,
-           $author,
-           $publisher,
-           $price;
+    public $title = 'title',
+           $author = 'author',
+           $publisher = 'publisher',
+           $price = 0;
 }
 
 $product1 = new Product();
+$product1 -> title = 'Naruto';
+$product1 -> author = 'Masashi Kishimoto';
+$product1 -> publisher = 'Shonen Jump';
+$product1 -> price = 30000;
+
 var_dump($product1);
 
 ?>
